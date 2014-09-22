c6c6960d3d52db01719898081ce5798fb168789a
XavierMeunier <meunier.xavier22@gmail.com>
2014-09-23 00:13:42 +0200

    [Naming] Change welcome to facebook_page

---
+++ app/controllers/facebook_pages_controller.rb
@@ -0,0 +1,32 @@
+class FacebookPagesController < ApplicationController
+
+  def index
+    @fb_page = FacebookPage.find_by_fb_id(params[:fb_id]) if params[:fb_id]
+    @fb_pages = FacebookPage.last(5).reverse
+    respond_to do |format|
+      format.html
+      format.json { render json: @fb_pages }
+    end
+  end
+  
+  def show
+    @fb_page = FacebookPage.find(params[:id])
+    @feeds = @fb_page.search_feeds
+    respond_to do |format|
+      format.html
+      format.json { render json: @fb_pages }
+    end
+  end
+
+  def page_search
+    @fb_page = FacebookPage.find_by_fb_id(params[:fb_id])
+    @fb_page = FacebookPage.search_page(params[:fb_id]) if @fb_page.blank?
+    
+    if @fb_page.is_a?(FacebookPage)
+      redirect_to index_path(@fb_page.fb_id)
+    else
+      redirect_to root_path, alert: "No Facebook page found."
+    end
+  end
+
+end
