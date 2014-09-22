8e8f7c97b7e2827dc3dbc07118a2159d7b2a61eb
XavierMeunier <meunier.xavier22@gmail.com>
2014-09-23 00:14:31 +0200

    [Structure] Adjust application files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def self.valid_json?(json_)
+    begin
+      json_ = json_.to_s
+      JSON.parse(json_)
+      return true
+    rescue JSON::ParserError
+      return false
+    end
+  end
+
+  def self.valid_xml?(xml_)
+    begin
+      Hash.from_xml(xml_)
+      return true
+    rescue
+      return false
+    end
+  end
+
 end
