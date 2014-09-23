1d19c12c0cf998775a799740d1940266dc9a48e4
XavierMeunier <meunier.xavier22@gmail.com>
2014-09-23 14:54:24 +0200

    [Views] Add assets fonts to assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,21 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module PageSearch
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+
+    config.i18n.default_locale = :fr
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
